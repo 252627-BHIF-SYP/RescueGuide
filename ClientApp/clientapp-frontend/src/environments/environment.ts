9a30ec5f3e7f0830a04de5bdd9d90b1e93deb547
BaysoyRabbi <145426693+BaysoyRabbi@users.noreply.github.com>
2026-03-17 11:13:42 +0100

    Revert "Revert "added signalingURL""
    This reverts commit 8d7b2cddeab8f519ef9dedded44bbaf9f5401532.

--- ClientApp/clientapp-frontend/src/environments/environment.ts
+++ ClientApp/clientapp-frontend/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5273/api' 
+  apiUrl: 'http://localhost:5273/api',
+  signalingUrl: 'http://localhost:3000'
+
 };
--- ControlCenterApp/controlcenter-frontend/src/environments/environment.ts
+++ ControlCenterApp/controlcenter-frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5062/api' 
+  apiUrl: 'http://localhost:5062/api',
+  signalingUrl: 'http://localhost:3000'
 };
