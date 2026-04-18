3c02dc11168d8751a199d280d0978a9bd86e2ee6
ZaunerErik <zauner.erik@gmail.com>
2026-04-18 20:34:35 +0200

    habe ein grundgerüst für die alarmierung implementiert. #43

--- ControlCenterApp/controlcenter-frontend/src/app/instruction-menu/instruction-menu.ts
+++ ControlCenterApp/controlcenter-frontend/src/app/instruction-menu/instruction-menu.ts
@@ -14,6 +14,7 @@ import {Router} from '@angular/router';
 import {InstructionMenuService, Measure, Plan} from '../services/instruction-menu.service';
 import { AuthService } from '../services/auth.service';
 
+
 @Component({
   selector: 'app-instruction-menu',
   imports: [
