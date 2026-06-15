b6df601a37cae5794d025105ad98d8d8b91d7a96
KilianFroschauerHTL <146070318+KilianFroschauer@users.noreply.github.com>
2026-06-15 22:43:22 +0200

    Push for Test

---
+++ apps/web-cc/controlcenter-frontend/src/app/services/call-context.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+
+export interface PendingOffer {
+  sdp: any;
+  from: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class CallContextService {
+  private pendingOffer$ = new BehaviorSubject<PendingOffer | null>(null);
+  private acceptSubject = new Subject<void>();
+  private acceptedFlag$ = new BehaviorSubject<boolean>(false);
+
+  // Called by signaling layer when an offer arrives
+  setPendingOffer(sdp: any, from: string) {
+    this.pendingOffer$.next({ sdp, from });
+  }
+
+  // Observable to react when an offer becomes available
+  getPendingOffer$(): Observable<PendingOffer | null> {
+    return this.pendingOffer$.asObservable();
+  }
+
+  // Synchronous getter
+  getPendingOffer(): PendingOffer | null {
+    return this.pendingOffer$.getValue();
+  }
+
+  // Clear stored offer
+  clearPendingOffer() {
+    this.pendingOffer$.next(null);
+  }
+
+  // Called by UI when user accepts the call
+  acceptCall() {
+    this.acceptSubject.next();
+    this.acceptedFlag$.next(true);
+  }
+
+  // Components can subscribe to accept events
+  getAccept$(): Observable<void> {
+    return this.acceptSubject.asObservable();
+  }
+
+  // If a component mounts after accept happened, it can check this
+  getAccepted$(): Observable<boolean> {
+    return this.acceptedFlag$.asObservable();
+  }
+
+  getAcceptedValue(): boolean {
+    return this.acceptedFlag$.getValue();
+  }
+
+  clearAcceptedFlag() {
+    this.acceptedFlag$.next(false);
+  }
+}
+
+
